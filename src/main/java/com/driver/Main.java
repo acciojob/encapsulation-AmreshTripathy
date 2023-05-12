56174879e1264e997e624930ff0d9c061519f2c9
AmreshTripathy <amreshtripathy184@gmail.com>
2023-05-12 11:38:56 +0530

    Encapsulation Assignment Push-1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        // Created object of RWOnly
+        RWOnly obj = new RWOnly();
+
+        obj.setName("Amresh");
+
+        System.out.println(obj.getName());
+    }
 }
